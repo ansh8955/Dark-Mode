0314d1db648c9d5c2f4aa60ff253404d1221deb9
Ansh Upadhyay <upadhyayaansh094@gmail.com>
2024-01-13 22:37:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector(".circle-container").addEventListener("click", () => {
+    document.getElementById("body").classList.toggle("dark-mode");
+    document.querySelector(".circle-btn").classList.toggle("addLeft");
+  });
+  
